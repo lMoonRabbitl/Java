fe506e56277fd9aa5b412f46b032ec9e17a163bd
lMoonRabbitl <zlld.go@qq.com>
2023-10-04 11:48:31 +0800

    BIGDATA

---
+++ BigData/src/main/java/com/ll/bigdata/Main.java
@@ -0,0 +1,8 @@
+package com.ll.bigdata;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello git!");
+    }
+}
